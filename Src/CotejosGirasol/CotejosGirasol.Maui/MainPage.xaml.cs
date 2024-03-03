ee4c07da25dab8e50cd278ee43c0070c12e5b998
German Enrique Álvarez Velandia <german.alvarez@actsis.com>
2024-03-03 15:08:03 -0500

    Ticket#1: ➕ Creación de proyectos y estructura.

---
+++ Src/CotejosGirasol/CotejosGirasol.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CotejosGirasol.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
